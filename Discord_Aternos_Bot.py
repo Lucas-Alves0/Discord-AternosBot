eaac1bdcc07287a83485e22a5f88eeb7887cef6c
Lucas Aves <100840952+Lucas-Alves0@users.noreply.github.com>
2024-12-07 21:16:32 -0300

    Release V0 Discord_Aternos_Bot

--- Discord_Aternos_Bot.py
+++ Discord_Aternos_Bot.py
@@ -1,51 +1,92 @@
+from discord.app_commands import commands
+from python_aternos.atserver import ServerStartError
 from python_aternos import *
-from time import *
+import python_aternos as aternos
+from time import sleep
+import discord
+from discord.ext import commands
+from keep_alive import keep_alive
 
-atclient = Client()
-
+# Aternos instance
+atclient = aternos.Client()
 user = {
-    'username':'SrLks7162',
-    'password':'L6070al#'
+  'username': 'SrLks7162', 'password': 'L6070al#'
 }
-atclient.login(user['username'],user['password'])
-aternos = atclient.account
+atclient.login(user['username'], user['password'])
+atlog = atclient.account
 
-servs = aternos.list_servers()
+servs = atlog.list_servers()
 myserv = servs[1]
+myserv.fetch()
 
+# Discord instance
 
-try:
+# Criação de intents necessários
+intents = discord.Intents.default()
+intents.message_content = True # Caso precise ler o conteúdo da mensagem
+dcbot = commands.Bot(command_prefix="!", intents=intents)
+dctoken = {
+  'TOKEN':'MTMxMzY0NTM4NzkxMDc0NjEyMg.GSY92J.argtbVI8M3VvMjztAbojaj5ZcP1ka4Brc3gyqk'
+}
+@dcbot.event
+async def on_ready():
+  print(f'Bot logado como {dcbot.user}')
+
+@dcbot.event
+async def on_message(message):
+    if message.author == dcbot.user:
+      return
+
+    if 'olá aternos' in message.content.lower():
+      await message.channel.send(f'Olá {message.author.name}!\nVamos começar?\n\nCaso queira iniciar o servidor, digite "!iniciar".\n\nTambém tenho outros comandos, digite "!ajuda" para ver.')
+    
+    await dcbot.process_commands(message)
+  
+@dcbot.command(name='iniciar')
+async def iniciar(ctx):
+  myserv.fetch()
+  if myserv.status == 'online':
+    pass
+  else:
+    await ctx.send('Abrindo servidor...')
+
+  try:
     myserv.start(headstart=True)
     myserv.fetch()
-    print(myserv.status)
-    while myserv.status == 'waiting':
-        sleep(10)
-        try:
-            myserv.confirm()
-        except:
-            myserv.fetch()
-            print('The server still', myserv.status)
-    else:
-        while myserv.status != 'online':
-            sleep(10)
-            myserv.fetch()
-            print('The server is', myserv.status)
-        else:
-            myserv.fetch()
-            print('The server is', myserv.status)
-except:
-    while myserv.status == 'waiting':
-        sleep(10)
-        try:
-            myserv.confirm()
-        except:
-            myserv.fetch()
-            print('The server is ', myserv.status)
-    else:
-        while myserv.status != 'online':
-            sleep(10)
-            myserv.fetch()
-            print('The server is ', myserv.status)
-        else:
-            myserv.fetch()
-            print('The server is ', myserv.status)
+    await ctx.send(f'O servidor está iniciando, aguarde\n\nStatus de abertura do servidor: {myserv.status}')
+  except ServerStartError:
+    await ctx.send('O servidor já está iniciando')
+
+@dcbot.command(name='parar')
+async def parar(ctx):
+  myserv.fetch()
+  await ctx.send('Fechando servidor...')
+
+  try:
+    myserv.stop()
+  except Exception as e:
+    await ctx.send(f'Ocorreu um erro inesperado: {e}')
+  while myserv.status != 'offline':
+    myserv.fetch()
+    await ctx.send(f'O servidor está sendo fechado, aguarde\n\nStatus de fechamento do servidor: {myserv.status}')
+  else:
+    sleep(15)
+    myserv.fetch()
+    await ctx.send(f'Servidor: {myserv.status}')
+
+@dcbot.command(name='status')
+async def status(ctx):
+  myserv.fetch()
+  await ctx.send(f'O servidor está {myserv.status}')
+
+@dcbot.command(name='jogadores')
+async def jogadores(ctx):
+  myserv.fetch()
+  await ctx.send(f'Jogadores online:\n\n{myserv.players_list}\n\n{myserv.players_count} de {myserv.slots} jogadores online')
+
+@dcbot.command(name='ajuda')
+async def ajuda(ctx):
+  await ctx.send('Comandos:\n\n!iniciar - Inicia o servidor\n!parar - Para o servidor\n!status - Mostra o status do servidor\n!jogadores - Mostra os jogadores online\n!ajuda - Mostra os comandos')
+
+keep_alive()
+dcbot.run(dctoken['TOKEN'])
